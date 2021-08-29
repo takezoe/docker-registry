27a2f87b6dffada04659bf7068314a4770796b28
Naoki Takezoe <takezoe@gmail.com>
2021-08-30 01:07:09 +0900

    Implementing upload layers

--- src/main/scala/com/github/takezoe/docker/registry/MyScalatraServlet.scala
+++ src/main/scala/com/github/takezoe/docker/registry/MyScalatraServlet.scala
@@ -4,6 +4,10 @@ import com.github.takezoe.docker.registry.storage.DockerRegistryStorage
 import org.scalatra._
 import org.scalatra.json._
 import org.json4s.DefaultFormats
+import org.json4s.JsonAST.JString
+
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.{ServletRequest, ServletResponse}
 
 class MyScalatraServlet extends ScalatraServlet with JacksonJsonSupport {
   protected implicit lazy val jsonFormats = DefaultFormats
@@ -15,6 +19,7 @@ class MyScalatraServlet extends ScalatraServlet with JacksonJsonSupport {
 
   // API Version Check
   get("/v2/") {
+    println(s"${request.getMethod} ${request.getRequestURI}")
     contentType = formats("json")
     response.addHeader("Docker-Distribution-Api-Version", "registry/2.0")
     Ok(Map.empty)
@@ -36,7 +41,7 @@ class MyScalatraServlet extends ScalatraServlet with JacksonJsonSupport {
   }
 
   // Starting An Upload
-  post("/v2/:name/blobs/uploads") {
+  post("/v2/:name/blobs/uploads/") {
     val name   = params("name")
     val digest = params.get("digest")
 
@@ -58,6 +63,13 @@ class MyScalatraServlet extends ScalatraServlet with JacksonJsonSupport {
 
   // Existing Layers
   head("/v2/:name/blobs/:digest") {
+    response.addHeader("Docker-Distribution-Api-Version", "registry/2.0")
+    val name   = params("name")
+    val digest = params("digest")
+    storage.getLayer(name, digest) match {
+      case Some(_) => Ok()
+      case None    => NotFound()
+    }
   }
 
   // Upload Progress
@@ -66,7 +78,6 @@ class MyScalatraServlet extends ScalatraServlet with JacksonJsonSupport {
     val uuid = params("uuid")
     val progress = storage.getProgress(uuid)
     response.addHeader("Range", s"0-${progress}")
-
     NoContent()
   }
 
@@ -89,13 +100,31 @@ class MyScalatraServlet extends ScalatraServlet with JacksonJsonSupport {
   }
 
   // Chunked Upload
-  patch("/v2/:name/blobs/uploads") {
-    NotImplemented()
+  patch("/v2/:name/blobs/uploads/:uuid") {
+    val name   = params("name")
+    val uuid   = params("uuid")
+
+    // TODO
+    val cType  = request.getHeader("Content-Type")
+    val range  = request.getHeader("Content-Range")
+    val length = request.getHeader("Content-Length")
+    println(s"Content-Type: ${cType}")
+    println(s"Content-Range: ${range}")
+    println(s"Content-Length: ${length}")
+
+    val in = request.getInputStream
+    val size = storage.uploadLayer(name, uuid, in)
+
+    response.addHeader("Docker-Upload-UUID", uuid)
+    response.addHeader("Location", s"/v2/${name}/blobs/uploads/${uuid}")
+    response.addHeader("Content-Length", "0")
+    response.addHeader("Range", s"0-${size}")
+
+    Accepted()
   }
 
   // Canceling an Upload
   delete("/v2/:name/blobs/uploads/:uuid") {
-
   }
 
   // Deleting a Layer
@@ -105,14 +134,24 @@ class MyScalatraServlet extends ScalatraServlet with JacksonJsonSupport {
 
   // Pushing an Image Manifest
   put("/v2/:name/manifests/:reference") {
+    val name      = params("name")
+    val reference = params("reference")
+
     val in = request.getInputStream
 
     // TODO debug
     val bytes = new Array[Byte](in.available())
     in.read(bytes)
-    println(new String(bytes, "UTF-8"))
+    val str = new String(bytes, "UTF-8")
+    println(str)
 
-    NotImplemented()
+    val json = parse(str)
+    val digest = (json \ "config" \ "digest").asInstanceOf[JString].values
+
+    response.addHeader("Location", s"/v2/${name}/manifests/${reference}")
+    response.addHeader("Docker-Content-Digest", digest)
+
+    Created()
   }
 
   // Listing Repositories
@@ -134,5 +173,10 @@ class MyScalatraServlet extends ScalatraServlet with JacksonJsonSupport {
   // Deleting an Image
   delete("/v2/:name/manifests/:reference") {
     NotImplemented()
+  }
+
+  override def service(req: ServletRequest, res: ServletResponse): Unit = {
+    println(s"${req.asInstanceOf[HttpServletRequest].getMethod} ${req.asInstanceOf[HttpServletRequest].getRequestURI}")
+    super.service(req, res)
   }
 }
--- src/main/scala/com/github/takezoe/docker/registry/storage/DockerRegistryStorage.scala
+++ src/main/scala/com/github/takezoe/docker/registry/storage/DockerRegistryStorage.scala
@@ -18,19 +18,31 @@ class DockerRegistryStorage {
     uuid
   }
 
-  def uploadLayer(name: String, uuid: String, in: InputStream): Unit = {
+  def uploadLayer(name: String, uuid: String, in: InputStream): Int = {
+    var size = 0
     Using.resource(new FileOutputStream(new File(s"${dataDir}/${name}-${uuid}"), true)){ out =>
       val bytes = new Array[Byte](81024 * 8)
       var length = 0
       while({length = in.read(bytes); length != -1 }) {
         out.write(bytes, 0, length)
+        size = size + length
         progress.get(uuid).add(length)
       }
     }
+    size
   }
 
   def getProgress(uuid: String): Long = {
     Option(progress.get(uuid)).map(_.longValue()).getOrElse(0L)
+  }
+
+  def getLayer(name: String, digest: String): Option[File] = {
+    val file = new File(s"${dataDir}/${name}/${digest}")
+    if(file.exists()) {
+      Some(file)
+    } else {
+      None
+    }
   }
 
   def finishUploadLayer(name: String, uuid: String, digest: String): Unit = {
